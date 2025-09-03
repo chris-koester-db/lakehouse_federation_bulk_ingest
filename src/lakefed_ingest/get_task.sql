766b1c01e7c6660a0dac8e0b3bac9f32345fe671
chris-koester-db <chris-koester-db>
2025-09-03 14:03:44 +0000

    Change incremental ingest to upsert

--- src/lakefed_ingest/get_task.sql
+++ src/lakefed_ingest/get_task.sql
@@ -1,7 +1,9 @@
 -- Get task metadata
 -- Workflow dynamic value references for SQL output are limited to 1,000 rows and 48 KB
--- Convert array to comma separated string for use with workflow dynamic value references
+-- Convert arrays to comma separated string for use with workflow dynamic value references (Arrays are not a supported type)
 select *
 from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
 where id = :task_id
-|> set sink_cluster_cols = case when array_size(sink_cluster_cols) > 0 then array_join(sink_cluster_cols, ', ') else null end
+|> set
+     primary_key = case when array_size(primary_key) > 0 then array_join(primary_key, ',') else null end,
+     sink_cluster_cols = case when array_size(sink_cluster_cols) > 0 then array_join(sink_cluster_cols, ',') else null end
